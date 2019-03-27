fe25f2d1998ca7bd458d488e8ba07bcf0d2034ac
ThePunisher754 <sanchezjose09754@hotmail.com>
2019-03-27 11:18:39 -0600

    Primer Commit
    prueba

---
+++ BotHackfest_27032019/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=botvideoshackfest-Web-Deploy.pubxml -p:Password=NXtu731SbFjJYtLJrvkldXEJDhydYM6pynjlLkCcqsHe5KES5Nb55SFx4fyA
+
